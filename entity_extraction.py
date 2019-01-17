c4455ae030bacfc3fb8933eae7029309fb843f99
Valerio Arnaboldi <valearna@caltech.edu>
2019-01-17 11:15:02 -0800

    modified pypdf2 function to add missing whitespaces in converted text

--- entity_extraction.py
+++ entity_extraction.py
@@ -2,6 +2,9 @@ import logging
 import re
 
 import PyPDF2 as PyPDF2
+from PyPDF2.generic import TextStringObject
+from PyPDF2.pdf import ContentStream, b_, FloatObject, NumberObject
+from PyPDF2.utils import u_
 from tqdm import tqdm
 import urllib.request
 from db_manager import DBManager
@@ -81,6 +84,44 @@ def get_first_valid_email_address_from_paper(fulltext, db_manager: DBManager):
 
 
 def get_fulltext_from_pdfs(pdfs_urls):
+
+    def customExtractText(self):
+        text = u_("")
+        content = self["/Contents"].getObject()
+        if not isinstance(content, ContentStream):
+            content = ContentStream(content, self.pdf)
+        # Note: we check all strings are TextStringObjects.  ByteStringObjects
+        # are strings where the byte->string encoding was unknown, so adding
+        # them to the text here would be gibberish.
+        for operands, operator in content.operations:
+            if operator == b_("Tj"):
+                _text = operands[0]
+                if isinstance(_text, TextStringObject):
+                    text += _text
+            elif operator == b_("T*"):
+                text += "\n"
+            elif operator == b_("'"):
+                text += "\n"
+                _text = operands[0]
+                if isinstance(_text, TextStringObject):
+                    text += operands[0]
+            elif operator == b_('"'):
+                _text = operands[2]
+                if isinstance(_text, TextStringObject):
+                    text += "\n"
+                    text += _text
+            elif operator == b_("TJ"):
+                for i in operands[0]:
+                    if isinstance(i, TextStringObject):
+                        text += i
+                    elif isinstance(i, FloatObject) or isinstance(i, NumberObject):
+                        if i < -100:
+                            text += " "
+            elif operator == b_("TD") or operator == b_("Tm"):
+                if len(text) > 0 and text[-1] != " " and text[-1] != "\n":
+                    text += " "
+        return text
+
     logger = logging.getLogger("AFP fulltext extraction")
     complete_fulltext = ""
     for pdf_url in pdfs_urls:
@@ -89,10 +130,11 @@ def get_fulltext_from_pdfs(pdfs_urls):
             with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
                 shutil.copyfileobj(response, tmp_file)
         try:
-            pdf_reader = PyPDF2.PdfFileReader(tmp_file.name)
+            pdf_reader = PyPDF2.PdfFileReader(open(tmp_file.name, 'rb'))
             for i in range(pdf_reader.numPages):
                 page_obj = pdf_reader.getPage(i)
-                pdf_fulltext += page_obj.extractText()
+                page_obj.extractText = customExtractText
+                pdf_fulltext += page_obj.extractText(page_obj)
         except:
             pass
         sentences = pdf_fulltext.split("\n")
@@ -102,5 +144,3 @@ def get_fulltext_from_pdfs(pdfs_urls):
             logger.info("Skipping response to reviewers")
     complete_fulltext = complete_fulltext.replace("\n", " ")
     return complete_fulltext
-
-
