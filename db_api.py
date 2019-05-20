73308b23e47d7927bfa3f1f7ec55ef469ed70f4f
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2019-05-20 09:51:02 -0700

    bug fixing
    check for null details in checkboxes

--- db_api.py
+++ db_api.py
@@ -170,36 +170,36 @@ class StorageEngine(object):
     def get_all_flagged_data_types(self, paper_id):
         svm_seqchange = self.db_manager.get_svm_value("seqchange", paper_id)
         afp_seqchange = self.db_manager.get_feature("afp_seqchange", paper_id)
-        afp_seqchange_checked = afp_seqchange != ""
+        afp_seqchange_checked = afp_seqchange != "" and afp_seqchange != "null"
         afp_seqchange_details = afp_seqchange if afp_seqchange != "Checked" and afp_seqchange != "checked" and \
                                                  afp_seqchange != "" else ""
         svm_geneint = self.db_manager.get_svm_value("geneint", paper_id)
         afp_geneint = self.db_manager.get_feature("afp_geneint", paper_id)
-        afp_geneint_checked = afp_geneint != ""
+        afp_geneint_checked = afp_geneint != "" and afp_geneint != "null"
         afp_geneint_details = afp_geneint if afp_geneint != "Checked" and afp_geneint != "checked" and \
                                              afp_geneint != "" else ""
         svm_geneprod = self.db_manager.get_svm_value("geneprod", paper_id)
         afp_geneprod = self.db_manager.get_feature("afp_geneprod", paper_id)
-        afp_geneprod_checked = afp_geneprod != ""
+        afp_geneprod_checked = afp_geneprod != "" and afp_geneprod != "null"
         afp_geneprod_details = afp_geneprod if afp_geneprod != "Checked" and afp_geneprod != "checked" and \
                                                afp_geneprod != "" else ""
         svm_genereg = self.db_manager.get_svm_value("genereg", paper_id)
         afp_genereg = self.db_manager.get_feature("afp_genereg", paper_id)
-        afp_genereg_checked = afp_genereg != ""
+        afp_genereg_checked = afp_genereg != "" and afp_genereg != "null"
         afp_genereg_details = afp_genereg if afp_genereg != "Checked" and afp_genereg != "checked" and \
                                              afp_genereg != "" else ""
         svm_newmutant = self.db_manager.get_svm_value("newmutant", paper_id)
         afp_newmutant = self.db_manager.get_feature("afp_newmutant", paper_id)
-        afp_newmutant_checked = afp_newmutant != ""
+        afp_newmutant_checked = afp_newmutant != "" and afp_newmutant != "null"
         afp_newmutant_details = afp_newmutant if afp_newmutant != "Checked" and afp_newmutant != "checked" and \
                                                  afp_newmutant != "" else ""
         svm_rnai = self.db_manager.get_svm_value("rnai", paper_id)
         afp_rnai = self.db_manager.get_feature("afp_rnai", paper_id)
-        afp_rnai_checked = afp_rnai != ""
+        afp_rnai_checked = afp_rnai != "" and afp_rnai != "null"
         afp_rnai_details = afp_rnai if afp_rnai != "Checked" and afp_rnai != "checked" and afp_rnai != "" else ""
         svm_overexpr = self.db_manager.get_svm_value("overexpr", paper_id)
         afp_overexpr = self.db_manager.get_feature("afp_overexpr", paper_id)
-        afp_overexpr_checked = afp_overexpr != ""
+        afp_overexpr_checked = afp_overexpr != "" and afp_overexpr != "null"
         afp_overexpr_details = afp_overexpr if afp_overexpr != "Checked" and afp_overexpr != "checked" and \
                                                afp_overexpr != "" else ""
         return {"svm_seqchange_checked": svm_seqchange, "afp_seqchange_checked": afp_seqchange_checked,
@@ -216,39 +216,39 @@ class StorageEngine(object):
 
     def get_all_yes_no_data_types(self, paper_id):
         afp_modchange = self.db_manager.get_feature("afp_structcorr", paper_id)
-        afp_modchange_checked = afp_modchange != ""
+        afp_modchange_checked = afp_modchange != "" and afp_modchange != "null"
         afp_modchange_details = afp_modchange if afp_modchange != "Checked" and afp_modchange != "checked" and \
                                                  afp_modchange != "" else ""
         afp_newantibody = self.db_manager.get_feature("afp_antibody", paper_id)
-        afp_newantibody_checked = afp_newantibody != ""
+        afp_newantibody_checked = afp_newantibody != "" and afp_newantibody != "null"
         afp_newantibody_details = afp_newantibody if afp_newantibody != "Checked" and afp_newantibody != "checked" and \
                                                      afp_newantibody != "" else ""
         afp_siteaction = self.db_manager.get_feature("afp_siteaction", paper_id)
-        afp_siteaction_checked = afp_siteaction != ""
+        afp_siteaction_checked = afp_siteaction != "" and afp_siteaction != "null"
         afp_siteaction_details = afp_siteaction if afp_siteaction != "Checked" and afp_siteaction != "checked" and \
                                                    afp_siteaction != "" else ""
         afp_timeaction = self.db_manager.get_feature("afp_timeaction", paper_id)
-        afp_timeaction_checked = afp_timeaction != ""
+        afp_timeaction_checked = afp_timeaction != "" and afp_timeaction != "null"
         afp_timeaction_details = afp_timeaction if afp_timeaction != "Checked" and afp_timeaction != "checked" and \
                                                    afp_timeaction != "" else ""
         afp_rnaseq = self.db_manager.get_feature("afp_rnaseq", paper_id)
-        afp_rnaseq_checked = afp_rnaseq != ""
+        afp_rnaseq_checked = afp_rnaseq != "" and afp_rnaseq != "null"
         afp_rnaseq_details = afp_rnaseq if afp_rnaseq != "Checked" and afp_rnaseq != "checked" and \
                                            afp_rnaseq != "" else ""
         afp_chemphen = self.db_manager.get_feature("afp_chemphen", paper_id)
-        afp_chemphen_checked = afp_chemphen != ""
+        afp_chemphen_checked = afp_chemphen != "" and afp_chemphen != "null"
         afp_chemphen_details = afp_chemphen if afp_chemphen != "Checked" and afp_chemphen != "checked" and \
                                                afp_chemphen != "" else ""
         afp_envpheno = self.db_manager.get_feature("afp_envpheno", paper_id)
-        afp_envpheno_checked = afp_envpheno != ""
+        afp_envpheno_checked = afp_envpheno != "" and afp_envpheno != "null"
         afp_envpheno_details = afp_envpheno if afp_envpheno != "Checked" and afp_envpheno != "checked" and \
                                                afp_envpheno != "" else ""
         afp_catalyticact = self.db_manager.get_feature("afp_catalyticact", paper_id)
-        afp_catalyticact_checked = afp_catalyticact != ""
+        afp_catalyticact_checked = afp_catalyticact != "" and afp_catalyticact != "null"
         afp_catalyticact_details = afp_catalyticact if afp_catalyticact != "Checked" and afp_catalyticact != \
                                                        "checked" and afp_catalyticact != "" else ""
         afp_humdis = self.db_manager.get_feature("afp_humdis", paper_id)
-        afp_humdis_checked = afp_humdis != ""
+        afp_humdis_checked = afp_humdis != "" and afp_humdis != "null"
         afp_humdis_details = afp_humdis if afp_humdis != "Checked" and afp_humdis != "checked" and \
                                            afp_humdis != "" else ""
         afp_additionalexpr = self.db_manager.get_feature("afp_additionalexpr", paper_id)
