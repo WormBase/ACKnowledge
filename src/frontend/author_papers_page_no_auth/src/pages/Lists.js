b8f866e10374601d02fd0d3f520f2dffbb142f6e
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2019-07-18 12:53:13 -0700

    added badges with numbers to tabs
    #113

--- src/frontend/author_papers_page_no_auth/src/pages/Lists.js
+++ src/frontend/author_papers_page_no_auth/src/pages/Lists.js
@@ -2,15 +2,20 @@ import React from 'react';
 import {Col, Container, Row, Tab, Tabs} from "react-bootstrap";
 import Message from "../components/Message";
 import PaginatedSearchList from "../components/PaginatedSearchList";
+import Badge from "react-bootstrap/Badge";
 
 class Lists extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            token_valid: undefined
+            token_valid: undefined,
+            num_waiting: 0,
+            num_completed: 0,
+            num_partial: 0
         };
 
         this.getPapersFromToken = this.getPapersFromToken.bind(this);
+        this.getTitleBadgeNum = this.getTitleBadgeNum.bind(this);
     }
 
     componentDidMount() {
@@ -37,7 +42,12 @@ class Lists extends React.Component {
                 if (data === undefined) {
                 } else {
                     if (data["token_valid"] === "True") {
-                        this.setState({token_valid: true})
+                        this.setState({
+                            token_valid: true
+                        });
+                        this.getTitleBadgeNum("get_processed_papers", 1);
+                        this.getTitleBadgeNum("get_submitted_papers", 2);
+                        this.getTitleBadgeNum("get_partial_papers", 3);
                     } else {
                         this.setState({token_valid: false})
                     }
@@ -48,7 +58,53 @@ class Lists extends React.Component {
         }
     }
 
+    getTitleBadgeNum(endpoint, index) {
+        let payload = {
+                from: 0,
+                count: 1,
+                passwd: this.props.token
+            };
+            fetch(process.env.REACT_APP_API_DB_READ_ENDPOINT + "/" + endpoint, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'text/html',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload)
+            }).then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                } else {
+                    alert("Error")
+                }
+            }).then(data => {
+                if (data === undefined) {
+                    alert("Empty response")
+                }
+                switch (index) {
+                    case 1: {
+                        this.setState({num_waiting: data["total_num_ids"]});
+                        break;
+                    }
+                    case 2: {
+                        this.setState({num_completed: data["total_num_ids"]});
+                        break;
+                    }
+                    case 3: {
+                        this.setState({num_partial: data["total_num_ids"]});
+                        break;
+                    }
+                }
+
+            }).catch((err) => {
+                alert(err);
+            });
+    }
+
     render() {
+        let waitingsub_title = <span>Papers waiting for data submission <Badge variant="danger">{this.state.num_waiting}</Badge></span>;
+        let completed_title = <span>Data submission completed <Badge variant="success">{this.state.num_completed}</Badge></span>;
+        let partial_title = <span>Partial data submission <Badge variant="warning">{this.state.num_partial}</Badge></span>;
         let content = "";
         if (this.state.token_valid) {
             content =
@@ -71,15 +127,15 @@ class Lists extends React.Component {
                     <Row>
                         <Col sm="12">
                             <Tabs defaultActiveKey="1" id="uncontrolled-tab-example">
-                                <Tab eventKey="1" title="Papers waiting for data submission">
+                                <Tab eventKey="1" title={waitingsub_title}>
                                     <PaginatedSearchList endpoint="get_processed_papers" papersPerPage="10"
                                                          passwd={this.props.token}/>
                                 </Tab>
-                                <Tab eventKey="2" title="Data submission completed">
+                                <Tab eventKey="2" title={completed_title}>
                                     <PaginatedSearchList endpoint="get_submitted_papers" papersPerPage="10"
                                                          passwd={this.props.token}/>
                                 </Tab>
-                                <Tab eventKey="3" title="Partial data submission">
+                                <Tab eventKey="3" title={partial_title}>
                                     <PaginatedSearchList endpoint="get_partial_papers" papersPerPage="10"
                                                          passwd={this.props.token}/>
                                 </Tab>
