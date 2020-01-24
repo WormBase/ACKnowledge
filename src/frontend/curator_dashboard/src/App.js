19af29b2f9be7ebe137fb6333f9f0dfcdd75fc09
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2020-01-24 10:14:48 -0800

    feature: added development banner

--- src/frontend/curator_dashboard/src/App.js
+++ src/frontend/curator_dashboard/src/App.js
@@ -4,13 +4,18 @@ import Main from './main_layout/Main'
 
 class App extends Component {
   render() {
-    return (
-        <div>
-            <Router>
-                <Main/>
-            </Router>
-        </div>
-    );
+      let developmentBanner = "";
+      if (process.env.NODE_ENV === "development") {
+          developmentBanner = <div id="devBanner"><h3>Development Site</h3></div>;
+      }
+      return (
+          <div>
+              {developmentBanner}
+              <Router>
+                  <Main/>
+              </Router>
+          </div>
+      );
   }
 }
 
