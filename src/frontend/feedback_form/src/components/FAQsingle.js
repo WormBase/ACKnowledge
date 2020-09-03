1e38c8a217bde556f900aec48ebfa0c48a462410
Valerio Arnaboldi <valearna@caltech.edu>
2020-09-03 13:19:02 -0700

    fix(FAQ): FAQ links now show 'hand' pointer

--- src/frontend/feedback_form/src/components/FAQsingle.js
+++ src/frontend/feedback_form/src/components/FAQsingle.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Panel from "react-bootstrap/lib/Panel";
-import Button from "react-bootstrap/lib/Button";
 import Collapse from "react-bootstrap/lib/Collapse";
 import Well from "react-bootstrap/lib/Well";
 
@@ -16,7 +14,7 @@ class FAQsingle extends React.Component {
     render(){
         return (
             <div>
-                <a onClick={() => this.setState({ open: !this.state.open })}>{this.props.question}</a>
+                <a onClick={() => this.setState({ open: !this.state.open })} href="#">{this.props.question}</a>
                 <Collapse in={this.state.open}>
                     <div>
                         <Well>
