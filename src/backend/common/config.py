baa638e8b4d264c161faa6a4df2b5d9ec820e7bd
Valerio Arnaboldi <valearna@caltech.edu>
2020-09-16 12:26:31 -0700

    fix(config): read config file path dynamically
    added definition file with variable set to path of project backend root folder

--- src/backend/common/config.py
+++ src/backend/common/config.py
@@ -2,12 +2,14 @@ import os
 import yaml
 import logging
 
+from src.backend.definitions import ROOT_DIR
+
 
 logger = logging.getLogger(__name__)
 
 
 def load_config_from_file():
-    with open(os.path.join(os.getcwd(), "src/backend/config.yml"), 'r') as stream:
+    with open(os.path.join(ROOT_DIR, 'config.yml'), 'r') as stream:
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as exc:
