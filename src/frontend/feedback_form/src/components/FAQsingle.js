349bcb65d3043f4a1f8a7bc6c12ca5c5a10e0e1f
Valerio Arnaboldi <valearna@caltech.edu>
2021-04-21 16:52:20 -0700

    refactor(react): FAQSingle rewritten with react hooks

--- src/frontend/feedback_form/src/components/FAQsingle.js
+++ src/frontend/feedback_form/src/components/FAQsingle.js
@@ -1,33 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Collapse from "react-bootstrap/lib/Collapse";
 import Well from "react-bootstrap/lib/Well";
 
-class FAQsingle extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            open: false
-        };
-    }
+const FAQsingle = (props) => {
 
+    const [open, setOpen] = useState(false);
 
-    render(){
-        return (
-            <div>
-                <a onClick={() => this.setState({ open: !this.state.open })}><p dangerouslySetInnerHTML={{__html: this.props.question}}/></a>
-                <Collapse in={this.state.open}>
-                    <div>
-                        <Well>
-                            <p dangerouslySetInnerHTML={{__html: this.props.answer}}/>
-                        </Well>
-                    </div>
-                </Collapse>
-            </div>
-
-        );
-    }
+    return (
+        <div>
+            <a onClick={() => setOpen(!open)}><p dangerouslySetInnerHTML={{__html: props.question}}/></a>
+            <Collapse in={open}>
+                <div>
+                    <Well>
+                        <p dangerouslySetInnerHTML={{__html: props.answer}}/>
+                    </Well>
+                </div>
+            </Collapse>
+        </div>
+    );
 }
 
 export default FAQsingle;
-
-
