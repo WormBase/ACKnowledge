bc4dd89556bd671dcec8e411189a5f4cf46f5e7a
Valerio Arnaboldi <valearna@caltech.edu>
2021-04-22 10:23:30 -0700

    refactor(react): InstructionsAlert rewritten with react hooks

--- src/frontend/feedback_form/src/main_layout/InstructionsAlert.js
+++ src/frontend/feedback_form/src/main_layout/InstructionsAlert.js
@@ -1,44 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import {Alert} from "react-bootstrap";
 
-class InstructionsAlert extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            alertTextNotSaved: props["alertTextNotSaved"],
-            alertTextSaved: props["alertTextSaved"],
-            alertTitleNotSaved: props["alertTitleNotSaved"],
-            alertTitleSaved: props["alertTitleSaved"],
-            alertBsStyleNotSaved: "info",
-            alertBsStyleSaved: "success",
-            show: true
-        };
+const InstructionsAlert = (props) => {
 
-        this.handleDismiss = this.handleDismiss.bind(this);
+    const [show, setShow] = useState(true);
+
+    let title = props["alertTitleNotSaved"];
+    let text = props["alertTextNotSaved"];
+    let bsstyle = "info";
+    if (props.saved) {
+        title = props["alertTitleSaved"];
+        text = props["alertTextSaved"];
+        bsstyle = "success";
     }
-
-    handleDismiss() {
-        this.setState({ show: false });
-    }
-
-    render() {
-        let title = this.state.alertTitleNotSaved;
-        let text = this.state.alertTextNotSaved;
-        let bsstyle = this.state.alertBsStyleNotSaved;
-        if (this.props.saved) {
-            title = this.state.alertTitleSaved;
-            text = this.state.alertTextSaved;
-            bsstyle = this.state.alertBsStyleSaved;
-        }
-        if (this.state.show) {
-            return (
-                <Alert onDismiss={this.handleDismiss} bsStyle={bsstyle}>
-                    <strong>{title}</strong><p>{text}</p>
-                </Alert>
-            );
-        } else {
-            return "";
-        }
+    if (show) {
+        return (
+            <Alert onDismiss={() => setShow(false)} bsStyle={bsstyle}>
+                <strong>{title}</strong><p>{text}</p>
+            </Alert>
+        );
+    } else {
+        return "";
     }
 }
 
