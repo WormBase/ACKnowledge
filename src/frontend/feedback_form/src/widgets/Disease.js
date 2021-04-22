9e58c8901f8ca62259979703f5d2391f0dc38d83
Valerio Arnaboldi <valearna@caltech.edu>
2021-04-22 11:54:22 -0700

    refactor(react): Disease widget rewritten with react hooks

--- src/frontend/feedback_form/src/widgets/Disease.js
+++ src/frontend/feedback_form/src/widgets/Disease.js
@@ -13,76 +13,73 @@ import {getPaperPassword} from "../redux/selectors/paperSelectors";
 import {saveWidgetData} from "../redux/actions/widgetActions";
 import {WIDGET} from "../constants";
 
-class Disease extends React.Component {
+const Disease = (props) => {
 
-    render() {
-
-        return (
-            <div>
-                <InstructionsAlert
-                    alertTitleNotSaved=""
-                    alertTitleSaved="Well done!"
-                    alertTextNotSaved="If this paper reports a disease model, please choose one or more that it describes."
-                    alertTextSaved="The data for this page has been saved, you can modify it any time."
-                    saved={this.props.isSavedToDB}
-                />
-                <Panel>
-                    <Panel.Heading>
-                        <Panel.Title componentClass="h3">Disease model data in the paper</Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body>
-                        <Form>
-                            <Checkbox checked={this.props.disease.checked} onClick={() => this.props.toggleDisease()}>
-                                <strong>The paper describes an experimental model for a specific human disease (e.g., Parkinson’s disease) by employing at least one of the following:</strong>
-                            </Checkbox>
-                            <ul>
-                                <li><strong>Gene/allele or strain that recapitulates disease phenotype(s) and provides insight into disease pathogenesis. Please provide official names for strains (e.g., CL2006).</strong></li>
-                                <li><strong>Transgenic studies with human and/or worm disease relevant gene.</strong></li>
-                                <li><strong>Modifiers of a new or previously established disease model (e.g., drugs, herbals, chemicals, etc).</strong></li>
-                            </ul>
-                        </Form>
-                    </Panel.Body>
-                </Panel>
-                <Panel>
-                    <Panel.Heading>
-                        <Panel.Title componentClass="h3">
-                            Additional comments on disease models in the paper
-                        </Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body>
-                        <div className="container-fluid">
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    Write comments here
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    <FormControl componentClass="textarea" multiple
-                                                 value={this.props.disease.details}
-                                                 onClick={() => this.props.setDisease(true, this.props.disease.details)}
-                                                 onChange={(event) => {
-                                                     this.props.setDisease(true, event.target.value);
-                                                 }}
-                                    />
-                                </div>
+    return (
+        <div>
+            <InstructionsAlert
+                alertTitleNotSaved=""
+                alertTitleSaved="Well done!"
+                alertTextNotSaved="If this paper reports a disease model, please choose one or more that it describes."
+                alertTextSaved="The data for this page has been saved, you can modify it any time."
+                saved={props.isSavedToDB}
+            />
+            <Panel>
+                <Panel.Heading>
+                    <Panel.Title componentClass="h3">Disease model data in the paper</Panel.Title>
+                </Panel.Heading>
+                <Panel.Body>
+                    <Form>
+                        <Checkbox checked={props.disease.checked} onClick={() => props.toggleDisease()}>
+                            <strong>The paper describes an experimental model for a specific human disease (e.g., Parkinson’s disease) by employing at least one of the following:</strong>
+                        </Checkbox>
+                        <ul>
+                            <li><strong>Gene/allele or strain that recapitulates disease phenotype(s) and provides insight into disease pathogenesis. Please provide official names for strains (e.g., CL2006).</strong></li>
+                            <li><strong>Transgenic studies with human and/or worm disease relevant gene.</strong></li>
+                            <li><strong>Modifiers of a new or previously established disease model (e.g., drugs, herbals, chemicals, etc).</strong></li>
+                        </ul>
+                    </Form>
+                </Panel.Body>
+            </Panel>
+            <Panel>
+                <Panel.Heading>
+                    <Panel.Title componentClass="h3">
+                        Additional comments on disease models in the paper
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-sm-12">
+                                Write comments here
                             </div>
                         </div>
-                    </Panel.Body>
-                </Panel>
-                <div align="right">
-                    <Button bsStyle="success" onClick={() => {
-                        let payload = {
-                            disease: getCheckboxDBVal(this.props.disease.checked, this.props.disease.details),
-                            passwd: this.props.paperPasswd
-                        };
-                        this.props.saveWidgetData(payload, WIDGET.DISEASE);
-                        }}>Save and continue
-                    </Button>
-                </div>
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <FormControl componentClass="textarea" multiple
+                                             value={props.disease.details}
+                                             onClick={() => props.setDisease(true, props.disease.details)}
+                                             onChange={(event) => {
+                                                 props.setDisease(true, event.target.value);
+                                             }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </Panel.Body>
+            </Panel>
+            <div align="right">
+                <Button bsStyle="success" onClick={() => {
+                    let payload = {
+                        disease: getCheckboxDBVal(props.disease.checked, props.disease.details),
+                        passwd: props.paperPasswd
+                    };
+                    props.saveWidgetData(payload, WIDGET.DISEASE);
+                }}>Save and continue
+                </Button>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 const mapStateToProps = state => ({
