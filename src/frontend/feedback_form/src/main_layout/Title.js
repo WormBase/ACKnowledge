5071d4d5cf95940a79978c0dff0a0c460cf0a2fa
Valerio Arnaboldi <valearna@caltech.edu>
2021-04-22 11:45:48 -0700

    refactor(react): Title rewritten with react hooks

--- src/frontend/feedback_form/src/main_layout/Title.js
+++ src/frontend/feedback_form/src/main_layout/Title.js
@@ -1,51 +1,41 @@
 import React from 'react';
 
-class Title extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: props.title,
-            journal: props.journal
-        };
-    }
+const Title = (props) => {
 
-    static removePdfTags(string) {
+    const removePdfTags = (string) => {
         let regex = /(<([^>]+)>)/ig;
         return string.replace(regex, "")
     }
 
-    render() {
-
-        let title = "";
-        if (this.state.title !== "") {
-            if (this.props["pmid"] !== "" && this.props["pmid"] !== undefined) {
-                title = <h4 className="text-left"><a
-                    href={"https://www.ncbi.nlm.nih.gov/pubmed/" + this.props["pmid"]} target="_blank">{Title.removePdfTags(this.state.title)}</a>; {this.state.journal}
-                </h4>;
-            } else if (this.props["doi"] !== "" && this.props["doi"] !== undefined){
-                title = <h4 className="text-left"><a
-                    href={"https://doi.org/" + this.props["doi"]} target="_blank">{Title.removePdfTags(this.state.title)}</a>; {this.state.journal}
-                </h4>;
-            } else {
-                title = <h4 className="text-left">{Title.removePdfTags(this.state.title)}; {this.state.journal}</h4>;
-            }
+    let title = "";
+    if (props.title !== "") {
+        if (props["pmid"] !== "" && props["pmid"] !== undefined) {
+            title = <h4 className="text-left"><a
+                href={"https://www.ncbi.nlm.nih.gov/pubmed/" + props["pmid"]} target="_blank">{removePdfTags(props.title)}</a>; {props.journal}
+            </h4>;
+        } else if (props["doi"] !== "" && props["doi"] !== undefined){
+            title = <h4 className="text-left"><a
+                href={"https://doi.org/" + props["doi"]} target="_blank">{removePdfTags(props.title)}</a>; {props.journal}
+            </h4>;
+        } else {
+            title = <h4 className="text-left">{removePdfTags(props.title)}; {props.journal}</h4>;
         }
+    }
 
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-12">
-                        <h3 className="text-left">Congratulations on the publication of your paper!</h3>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        {title}
-                    </div>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12">
+                    <h3 className="text-left">Congratulations on the publication of your paper!</h3>
                 </div>
             </div>
-        );
-    }
+            <div className="row">
+                <div className="col-sm-12">
+                    {title}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Title;
