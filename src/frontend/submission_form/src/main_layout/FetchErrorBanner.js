6358c7ac75e8f2c9ad253cdffa925ac1a8886051
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-09-30 16:04:36 -0700

    fix(fetchErrorBanner): banner resized to have same size as main area

--- src/frontend/submission_form/src/main_layout/FetchErrorBanner.js
+++ src/frontend/submission_form/src/main_layout/FetchErrorBanner.js
@@ -5,17 +5,21 @@ import Glyphicon from "react-bootstrap/lib/Glyphicon";
 
 const FetchErrorBanner = () => {
     return (
-        <div>
-            {useSelector((state) => state.display.showDataFetchError) ?
-                <Alert bsStyle="danger">
-                    <Glyphicon glyph="warning-sign"/>
-                    <strong>Error</strong><br/>
-                    We are having problems retrieving your data from the server and some components may
-                    behave incorrectly. This could be caused by wrong credentials or by a network issue.
-                    Please try again later or contact <a href="mailto:help@wormbase.org">
-                    Wormbase Helpdesk</a>.
-                </Alert> : null
-            }
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12">
+                    {useSelector((state) => state.display.showDataFetchError) ?
+                        <Alert bsStyle="danger">
+                            <Glyphicon glyph="warning-sign"/>
+                            <strong>Error</strong><br/>
+                            We are having problems retrieving your data from the server and some components may
+                            behave incorrectly. This could be caused by wrong credentials or by a network issue.
+                            Please try again later or contact <a href="mailto:help@wormbase.org">
+                            Wormbase Helpdesk</a>.
+                        </Alert> : null
+                    }
+                </div>
+            </div>
         </div>
     )
 }
