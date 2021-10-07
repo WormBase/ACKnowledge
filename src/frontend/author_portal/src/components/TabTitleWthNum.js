d85d13d563cfe91668e5da92374c7df35df29374
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-10-07 16:30:34 -0700

    fix(author_portal): fixed badge colors in tabs

--- src/frontend/author_portal/src/components/TabTitleWthNum.js
+++ src/frontend/author_portal/src/components/TabTitleWthNum.js
@@ -6,23 +6,27 @@ import {Badge} from "react-bootstrap";
 const TabTitleWthNum = ({listType}) => {
     let totNumPapers;
     let title;
+    let variant;
     switch (listType) {
         case listTypes.WAITING:
             totNumPapers = useSelector((state) => state.lists.totNumWaiting);
             title = "Waiting for submission";
+            variant = "danger";
             break;
         case listTypes.PARTIAL:
             totNumPapers = useSelector((state) => state.lists.totNumPartial);
             title = "Partial submissions";
+            variant = "warning";
             break;
         case listTypes.SUBMITTED:
             totNumPapers = useSelector((state) => state.lists.totNumSubmitted);
-            title = "Completed submissions"
+            title = "Completed submissions";
+            variant = "success";
             break;
     }
 
     return (
-        <span>{title} <Badge variant="warning">{totNumPapers}</Badge></span>
+        <span>{title} <Badge variant={variant}>{totNumPapers}</Badge></span>
     );
 }
 
