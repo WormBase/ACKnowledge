96d8a944ea8b4a1e5c8a7d11000abed32adbb0f4
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-10-14 18:24:55 -0700

    feat(stats): added interval selection and fixed api

--- src/frontend/curator_dashboard/src/pages/Statistics.js
+++ src/frontend/curator_dashboard/src/pages/Statistics.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {withRouter} from "react-router-dom";
 import RespRateTSChart from "./stats/RespRateTSChart";
 import RespRateTotalPieCharts from "./stats/RespRateTotalPieCharts";
 
 
 const Statistics = () => {
+    const [binSize, setBinSize] = useState('y');
     return(
         <div>
             <RespRateTotalPieCharts/>
             <hr/>
-            <RespRateTSChart/>
+            <RespRateTSChart bin_size={binSize}/>
+            Interval period
+            <br/>
+            <select onChange={(event) => setBinSize(event.target.value)}>
+                <option value="y">1 year</option>
+                <option value="m">1 month</option>
+            </select>
         </div>
     );
 }
--- src/frontend/curator_dashboard/src/pages/stats/RespRateTSChart.js
+++ src/frontend/curator_dashboard/src/pages/stats/RespRateTSChart.js
@@ -4,9 +4,10 @@ import HighchartsReact from 'highcharts-react-official';
 import {useQuery} from "react-query";
 import axios from "axios";
 
-const RespRateTSChart = () => {
-    const data = useQuery('respRateTS', () =>
-        axios.post(process.env.REACT_APP_API_DB_READ_ADMIN_ENDPOINT + "/stats_timeseries", {bin_size: 'y'}))
+const RespRateTSChart = ({bin_size = 'y'}) => {
+    const data = useQuery('respRateTS' + bin_size, () =>
+        axios.post(process.env.REACT_APP_API_DB_READ_ADMIN_ENDPOINT + "/stats_timeseries", {bin_size: bin_size}))
+    const showYearOnly = bin_size.includes("y");
     const options = {
         title: {
             text: 'Response Rate over time'
@@ -22,18 +23,41 @@ const RespRateTSChart = () => {
                 enableMouseTracking: false
             }
         },
-        yAxis: {
-            title: {
-                text: 'Response rate %'
-            }
-        },
         xAxis: {
-            categories: data.isSuccess ? data.data.data.map(statArr => statArr[0]) : []
+            categories: data.isSuccess ? data.data.data.map(statArr => {
+                if (showYearOnly) {
+                    return statArr[0].split("-")[0];
+                } else {
+                    return statArr[0];
+                }
+            }) : []
         },
-        series: [{
-            name: "Response rate",
-            data: data.isSuccess ? data.data.data.map(statArr => statArr[1][4].toFixed(4) * 100) : []
-        }]
+        series: [
+            {
+                name: "Response rate (%)",
+                data: data.isSuccess ? data.data.data.map(statArr => parseFloat((statArr[1][4] * 100).toFixed(2))) : []
+            },
+            {
+                name: "Full submissions",
+                data: data.isSuccess ? data.data.data.map(statArr => statArr[1][1]) : [],
+                visible: false
+            },
+            {
+                name: "Partial submissions",
+                data: data.isSuccess ? data.data.data.map(statArr => statArr[1][2]) : [],
+                visible: false
+            },
+            {
+                name: "Processed but w/o submission",
+                data: data.isSuccess ? data.data.data.map(statArr => statArr[1][3]) : [],
+                visible: false
+            },
+            {
+                name: "Processed (total)",
+                data: data.isSuccess ? data.data.data.map(statArr => statArr[1][0]) : [],
+                visible: false
+            }
+        ]
     }
 
     return (
