0b6c048b73f8907b94cf2bc9c04327338c3d06b6
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-10-22 17:16:04 -0700

    refactor(dashboard): cleaned up comments

--- src/frontend/curator_dashboard/src/pages/paper_viewer/CommentsTab.js
+++ src/frontend/curator_dashboard/src/pages/paper_viewer/CommentsTab.js
@@ -1,78 +1,18 @@
 import React from 'react';
-import {withRouter} from "react-router-dom";
-import queryString from 'query-string/index';
-import {extractEntitiesFromTfpString} from "../../AFPValues";
-import LoadingOverlay from 'react-loading-overlay';
-import ManualEntityRow from "./tabs_area_components/ManualEntityRow";
+import axios from "axios";
+import {Spinner} from "react-bootstrap";
+import {useSelector} from "react-redux";
+import {useQuery} from "react-query";
 
-class CommentsTab extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.state = {
-            paper_id: queryString.parse(this.props.location.search).paper_id,
-            comments: "",
-            isLoading: false
-        };
-        this.loadDataFromAPI = this.loadDataFromAPI.bind(this);
-    }
+const CommentsTab = () => {
+    const paperID = useSelector((state) => state.paperID);
+    const queryRes = useQuery('paperComments' + paperID, () =>
+        axios.post(process.env.REACT_APP_API_DB_READ_ADMIN_ENDPOINT + "/comments", {paper_id: paperID}))
 
-    componentDidUpdate() {
-        if (this.state.paper_id !== queryString.parse(this.props.location.search).paper_id) {
-            this.loadDataFromAPI();
-            this.setState({paper_id: queryString.parse(this.props.location.search).paper_id});
-        }
-
-    }
-
-    componentDidMount() {
-        this.loadDataFromAPI();
-    }
-
-    loadDataFromAPI() {
-        let payload = {
-            paper_id: queryString.parse(this.props.location.search).paper_id
-        };
-        if (payload.paper_id !== undefined) {
-            this.setState({isLoading: true});
-            fetch(process.env.REACT_APP_API_DB_READ_ADMIN_ENDPOINT + "/comments", {
-                method: 'POST',
-                headers: {
-                    'Accept': 'text/html',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(payload)
-            }).then(res => {
-                if (res.status === 200) {
-                    return res.json();
-                } else {
-
-                }
-            }).then(data => {
-                if (data === undefined) {
-                }
-                this.setState({
-                    comments: data["afp_comments"],
-                    isLoading: false
-                });
-            }).catch((err) => {
-                alert(err);
-            });
-        }
-    }
-
-    render() {
-        return(
-            <LoadingOverlay
-                active={this.state.isLoading}
-                spinner
-                text='Loading paper data...'
-                styles={{
-                    overlay: (base) => ({
-                        ...base,
-                        background: 'rgba(65,105,225,0.5)'
-                    })
-                }}
-            >
+    return(
+        <div>
+            {queryRes.isLoading ? <Spinner animation="border"/> : null}
+            {queryRes.isSuccess ?
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-sm-12">
@@ -81,13 +21,13 @@ class CommentsTab extends React.Component {
                     </div>
                     <div className="row">
                         <div className="col-sm-12">
-                            {this.state.comments !== null && this.state.comments !== '' ? this.state.comments : 'Null'}
+                            {queryRes.data.data.afp_comments !== null && queryRes.data.data.afp_comments !== '' ? queryRes.data.data.afp_comments : 'Null'}
                         </div>
                     </div>
                 </div>
-            </LoadingOverlay>
-        );
-    }
+                : null}
+        </div>
+    );
 }
 
-export default withRouter(CommentsTab);
+export default CommentsTab;
