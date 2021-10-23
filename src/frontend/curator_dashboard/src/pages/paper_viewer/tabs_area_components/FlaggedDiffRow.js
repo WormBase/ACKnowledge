0baac6104de91aa41b060a51622f0cefdc66e4da
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2021-10-22 17:20:16 -0700

    refactor(dashboard): cleaned up components for single entries

--- src/frontend/curator_dashboard/src/pages/paper_viewer/tabs_area_components/FlaggedDiffRow.js
+++ src/frontend/curator_dashboard/src/pages/paper_viewer/tabs_area_components/FlaggedDiffRow.js
@@ -1,33 +1,30 @@
 import React from 'react';
 
-class FlaggedDiffRow extends React.Component {
-
-    render() {
-        return(
-            <div>
-                <div className="row">
-                    <div className="col-sm-3">
-                        {this.props.title}
-                    </div>
-                    <div className="col-sm-3">
-                        Checked: <strong>{this.props.tfpChecked}</strong>
-                    </div>
-                    <div className="col-sm-3">
-                        Checked: <strong>{this.props.afpDetails !== "'null'" ? this.props.afpChecked: ""}<br/></strong>
-                        Details: <strong>{this.props.afpDetails !== "'null'" ? this.props.afpDetails : ""}</strong>
-                    </div>
-                    <div className="col-sm-3">
-                        <strong>{this.props.afpDetails !== "'null'" && this.props.afpDetails !== "null"? this.props.tfpChecked !== this.props.afpChecked ? "Yes" : "No" : "No"}</strong>
-                    </div>
+const FlaggedDiffRow = ({title, tfpChecked, afpChecked, afpDetails}) => {
+    return(
+        <div>
+            <div className="row">
+                <div className="col-sm-3">
+                    {title}
                 </div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        <hr/>
-                    </div>
+                <div className="col-sm-3">
+                    Checked: <strong>{tfpChecked}</strong>
+                </div>
+                <div className="col-sm-3">
+                    Checked: <strong>{afpDetails !== "'null'" ? afpChecked: ""}<br/></strong>
+                    Details: <strong>{afpDetails !== "'null'" ? afpDetails : ""}</strong>
+                </div>
+                <div className="col-sm-3">
+                    <strong>{afpDetails !== "'null'" && afpDetails !== "null"? tfpChecked !== afpChecked ? "Yes" : "No" : "No"}</strong>
                 </div>
             </div>
-        );
-    }
+            <div className="row">
+                <div className="col-sm-12">
+                    <hr/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default FlaggedDiffRow;
--- src/frontend/curator_dashboard/src/pages/paper_viewer/tabs_area_components/FlaggedInfoRow.js
+++ src/frontend/curator_dashboard/src/pages/paper_viewer/tabs_area_components/FlaggedInfoRow.js
@@ -1,27 +1,24 @@
 import React from 'react';
 
-class FlaggedInfoRow extends React.Component {
-
-    render() {
-        return(
-            <div>
-                <div className="row">
-                    <div className="col-sm-6">
-                        {this.props.title}
-                    </div>
-                    <div className="col-sm-6">
-                        Checked: <strong>{this.props.afpChecked}<br/></strong>
-                        Details: <strong>{this.props.afpDetails}</strong>
-                    </div>
+const FlaggedInfoRow = ({title, afpChecked, afpDetails}) => {
+    return(
+        <div>
+            <div className="row">
+                <div className="col-sm-6">
+                    {title}
                 </div>
-                <div className="row">
-                    <div className="col-sm-12">
-                        <hr/>
-                    </div>
+                <div className="col-sm-6">
+                    Checked: <strong>{afpChecked}<br/></strong>
+                    Details: <strong>{afpDetails}</strong>
                 </div>
             </div>
-        );
-    }
+            <div className="row">
+                <div className="col-sm-12">
+                    <hr/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default FlaggedInfoRow;
